b31358bdd5f383335f3b83aaa0d7e5ad7a63b3bf
Justin <jpkusch@umich.edu>
2020-08-11 12:59:36 -0400

    start of making interfaces

--- SafeConsultation/src/main/java/dev/four/daos/PatientDAO.java
+++ SafeConsultation/src/main/java/dev/four/daos/PatientDAO.java
@@ -1,5 +1,20 @@
 package dev.four.daos;
 
+import java.util.List;
+
+import dev.four.entities.Patient;
+
 public interface PatientDAO {
+	
+	// Create
+	Patient createPatient(Patient patient);
+	
+	// Read
+	Patient getPatientById(int id);
+	List<Patient> getPatientsByDoctorId(int it);
+	Patient getPatientByUsername(String username);
+	
+	// Update
+	Patient updatePatient(Patient patient);
 
 }
