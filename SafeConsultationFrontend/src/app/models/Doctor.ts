2742a2ab6efe62162406b3991ed71e5af859b47a
Juliyan89 <juliyan.atanasov@gmail.com>
2020-08-17 17:11:13 -0400

    Appointment Service

---
+++ SafeConsultationFrontend/src/app/models/Doctor.ts
@@ -0,0 +1,6 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+export class Doctor { 
+}
--- SafeConsultationFrontend/src/app/services/appointment.service.ts
+++ SafeConsultationFrontend/src/app/services/appointment.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Appointment} from '../models/Appointment';
+import {Patient} from '../models/Patient'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AppointmentService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  async createAppointment(appointment: Appointment):Promise<Appointment>{
+      
+    const appoint:Appointment = await this.http.post<Appointment>("https://localhost:8080/appointments",JSON.stringify(appointment)).toPromise();
+    return appoint;
+  }
+
+  async getAppointmentById(aid: number):Promise<Appointment>{
+    const appoint:Appointment = await this.http.get<Appointment>(`https://localhost:8080/appointments/${aid}`).toPromise();
+    return appoint;
+  }
+
+
+  async updateAppointment(appointment: Appointment):Promise<Appointment>{
+    
+    const appoint:Appointment = await this.http.put<Appointment>("https://localhost:8080/appointments",JSON.stringify(appointment)).toPromise();
+    return appoint;
+  }
 }
