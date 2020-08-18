21c805d2fac44744c319a1f449f82577a734669e
jpkusch <42867909+jpkusch@users.noreply.github.com>
2020-08-18 16:40:29 -0400

    patientTab, docotrPage, accoutnTab

--- SafeConsultationFrontend/src/app/components/account-tab/account-tab.component.ts
+++ SafeConsultationFrontend/src/app/components/account-tab/account-tab.component.ts
@@ -1,4 +1,7 @@
+import { LoginService } from './../../services/login.service';
 import { Component, OnInit } from '@angular/core';
+import { Patient } from 'src/app/models/Patient';
+import { Doctor } from 'src/app/models/Doctor';
 
 @Component({
   selector: 'app-account-tab',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountTabComponent implements OnInit {
 
-  constructor() { }
+  doctor: Doctor = null;
+  patient: Patient = null;
+  isLoaded: boolean = false;
+
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.docPat();
+    this.isLoaded = true;
+  }
+
+  docPat(): void {
+    this.doctor = this.loginService.doctorUser;
+    this.patient = this.loginService.patientUser;
   }
 
 }
--- SafeConsultationFrontend/src/app/services/doctor.service.ts
+++ SafeConsultationFrontend/src/app/services/doctor.service.ts
@@ -12,37 +12,37 @@ export class DoctorService {
   constructor(private httpClient: HttpClient) { }
 
   async createDoctor(doctorIn: Doctor): Promise<Doctor> {
-    const url: string = 'http://localhost:8080/doctors';
+    const url: string = 'http://ec2-3-131-82-174.us-east-2.compute.amazonaws.com:8080/doctors';
     const doctor: Doctor = await this.httpClient.post<Doctor>(url, JSON.stringify(doctorIn)).toPromise();
     return doctor;
   }
 
   async getDoctorById(did: number): Promise<Doctor> {
-    const url: string = `http://localhost:8080/doctors/${did}`;
+    const url: string = `http://ec2-3-131-82-174.us-east-2.compute.amazonaws.com:8080/doctors/${did}`;
     const doctor: Doctor = await this.httpClient.get<Doctor>(url).toPromise();
     return doctor;
   }
 
   async getAllDoctors(): Promise<Doctor[]> {
-    const url: string = 'http://localhost:8080/doctors';
+    const url: string = 'http://ec2-3-131-82-174.us-east-2.compute.amazonaws.com:8080/doctors';
     const doctors: Doctor[] = await this.httpClient.get<Doctor[]>(url).toPromise();
     return doctors;
   }
 
   async updateDoctor(doctorIn: Doctor): Promise<Doctor> {
-    const url: string = 'http://localhost:8080/doctors';
+    const url: string = 'http://ec2-3-131-82-174.us-east-2.compute.amazonaws.com:8080/doctors';
     const doctor: Doctor = await this.httpClient.put<Doctor>(url, JSON.stringify(doctorIn)).toPromise();
     return doctor;
   }
 
   async getPatientsByDoctor(did: number): Promise<Patient[]> {
-    const url: string = `http://localhost:8080/doctors/${did}/patients`;
+    const url: string = `http://ec2-3-131-82-174.us-east-2.compute.amazonaws.com:8080/doctors/${did}/patients`;
     const patients: Patient[] = await this.httpClient.get<Patient[]>(url).toPromise();
     return patients;
   }
 
   async getAppointmentsByDoctor(did: number): Promise<Appointment[]> {
-    const url: string = `http://localhost:8080/doctors/${did}/appointments`;
+    const url: string = `http://ec2-3-131-82-174.us-east-2.compute.amazonaws.com:8080/doctors/${did}/appointments`;
     const appointments: Appointment[] = await this.httpClient.get<Appointment[]>(url).toPromise();
     return appointments;
   }
